ece5792a65f5e0883645be8cf3f0b69e25c3f0d9
becquerk29 <becquerk29@gmail.com>
2023-06-28 17:41:46 -0500

    fix port en db.js

--- config/db.js
+++ config/db.js
@@ -7,7 +7,8 @@ console.log(process.env.DB_HOST);
 
 const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
     host:process.env.DB_HOST,
-    port:'3306',
+    //port:'3306',
+    port:'5751',
     dialect:'mysql',
     define: {
         timestamps: false 
